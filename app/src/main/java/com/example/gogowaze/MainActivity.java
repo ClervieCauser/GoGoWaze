516152d098664e9ecf8d27716a98d6ef910ced2c
Julien ORIOL <julien@MacBook-Pro-de-Julien.local>
2023-04-11 15:01:08 +0200

    feat: ajout de la nav bar + background

--- app/src/main/java/com/example/gogowaze/MainActivity.java
+++ app/src/main/java/com/example/gogowaze/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.gogowaze;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.gogowaze.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //Rajouter ici le fragment par défaut
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
+                case R.id.stats:
+                    replaceFragment(new StatsFragment());
+                    break;
+                case R.id.warning:
+                    replaceFragment(new SignalFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case R.id.sos:
+                    replaceFragment(new EmergencyCallFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
