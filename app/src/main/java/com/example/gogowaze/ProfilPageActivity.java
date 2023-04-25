917cfb001895440ae2b996d8223b8690c78424a3
LEO MENDES-MELIN <leo.mendes-melin@hotmail.fr>
2023-04-25 18:04:01 +0200

    ajout de la fonctionnalité pour changer de photo de profil

--- app/src/main/java/com/example/gogowaze/ProfilPageActivity.java
+++ app/src/main/java/com/example/gogowaze/ProfilPageActivity.java
@@ -1,11 +1,24 @@
 package com.example.gogowaze;
 
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
@@ -13,11 +26,36 @@ import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.tabs.TabLayout;
 
 public class ProfilPageActivity extends AppCompatActivity {
-
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil_page);
+
+        // Obtenez une référence à l'ImageView
+        ImageView imageView = findViewById(R.id.imageView);
+
+        // Ajouter un OnClickListener à l'ImageView
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ouvrir l'appareil photo
+                // Vérifier si l'application a l'autorisation d'utiliser l'appareil photo
+                if (ContextCompat.checkSelfPermission(ProfilPageActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    // Demander la permission d'utiliser l'appareil photo
+                    ActivityCompat.requestPermissions(ProfilPageActivity.this, new String[]{Manifest.permission.CAMERA}, 1);
+                } else {
+                    // Ouvrir l'appareil photo
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+                }
+                //Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+               // startActivityForResult(intent, 1);
+            }
+        });
+
+
+
 
         // Obtenez une référence au TabLayout et au ViewPager dans votre activité ou votre fragment
         TabLayout tabLayout = findViewById(R.id.tab_layout);
@@ -65,5 +103,20 @@ public class ProfilPageActivity extends AppCompatActivity {
 
         // Attacher le ViewPager au TabLayout
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    // Gérer le résultat de la prise de photo
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            // Récupérer l'image capturée
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView imageView = findViewById(R.id.imageView);
+
+            // Mettre l'image capturée dans l'ImageView
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 }
