10e260a4e05a9f38accee28e40f32804d5008e99
amandinemart1 <amandine.martin@etu.unic-cotedazur.fr>
2023-05-03 09:57:44 +0200

    mise en place de la map en arriere plan

--- app/src/main/java/com/example/gogowaze/MainActivity.java
+++ app/src/main/java/com/example/gogowaze/MainActivity.java
@@ -4,20 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.preference.PreferenceManager;
 
 import android.os.Bundle;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.OverlayItem;
+
 import com.example.gogowaze.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MapView map;
     ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Configuration.getInstance().load( getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        map = findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK); // render
+        map.setBuiltInZoomControls(true); // zoom
+        GeoPoint startPoint = new GeoPoint(43.65020, 07.00517);
+        IMapController mapControler = map.getController();
+        mapControler.setZoom(18.0);
+        mapControler.setCenter(startPoint);
+
         //Rajouter ici le fragment par défaut
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
