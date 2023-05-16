bdad2ab9629219a7c6ec77414adf068bd23f6835
LEO MENDES-MELIN <leo.mendes-melin@hotmail.fr>
2023-05-16 18:26:47 +0200

    axe 2 web service stat fonctionnel mais esthétique à améliorer

--- app/src/main/java/com/example/gogowaze/StatisticsActivity.java
+++ app/src/main/java/com/example/gogowaze/StatisticsActivity.java
@@ -1,6 +1,13 @@
 package com.example.gogowaze;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,9 +20,13 @@ import com.example.gogowaze.DownloadJsonTask;
 import com.example.gogowaze.OnDataLoadedListener;
 import com.example.gogowaze.TypeAccidentFragment;
 import com.google.android.material.tabs.TabLayout;
-
 public class StatisticsActivity extends AppCompatActivity implements OnDataLoadedListener {
     private AccidentData accidentData;
+    private FragmentPagerAdapter adapter;
+    private TypeAccidentFragment typeAccidentFragment;
+
+    private GraviteFragment graviteFragment;
+    private Button validerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +34,35 @@ public class StatisticsActivity extends AppCompatActivity implements OnDataLoade
         setContentView(R.layout.activity_statistique);
 
         new DownloadJsonTask(this).execute("http://bluedays.com/data/web/donnees.json");
+
+        validerButton = findViewById(R.id.valider);
+        validerButton.setEnabled(false);
+        validerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText searchEditText = findViewById(R.id.search_edit_text);
+                String city = searchEditText.getText().toString();
+                Log.d("ici", city);
+
+                if (typeAccidentFragment != null && graviteFragment != null) {
+                    // Obtenir le TableLayout du fragment TypeAccident
+                    TableLayout tableLayoutType = typeAccidentFragment.getView().findViewById(R.id.table_layout);
+
+                    // Appeler fillTableWithData pour TypeAccident
+                    typeAccidentFragment.fillTableWithData(tableLayoutType, city);
+
+                    // Obtenir le TableLayout du fragment Gravite
+                    TableLayout tableLayoutGravite = graviteFragment.getView().findViewById(R.id.table_layout);
+
+                    // Appeler fillTableWithData pour Gravite
+                    graviteFragment.fillTableWithData(tableLayoutGravite, city);
+                }
+                // Fermer le clavier
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(searchEditText.getWindowToken(), 0);
+            }
+        });
+
     }
 
     @Override
@@ -32,15 +72,17 @@ public class StatisticsActivity extends AppCompatActivity implements OnDataLoade
         TabLayout tabLayout = findViewById(R.id.tab_layout);
         ViewPager viewPager = findViewById(R.id.view_pager);
 
-        FragmentPagerAdapter adapter = new FragmentPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+        adapter = new FragmentPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
             @NonNull
             @Override
             public Fragment getItem(int position) {
                 switch (position) {
                     case 0:
-                        return TypeAccidentFragment.newInstance(accidentData);
+                        typeAccidentFragment = TypeAccidentFragment.newInstance(accidentData);
+                        return typeAccidentFragment;
                     case 1:
-                        return TypeAccidentFragment.newInstance(accidentData); // Remplacez par le fragment pour "gravite"
+                        graviteFragment = GraviteFragment.newInstance(accidentData);
+                        return graviteFragment; // Remplacez par le fragment pour "gravite"
                     default:
                         return null;
                 }
@@ -67,5 +109,6 @@ public class StatisticsActivity extends AppCompatActivity implements OnDataLoade
 
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+        validerButton.setEnabled(true);
     }
 }
