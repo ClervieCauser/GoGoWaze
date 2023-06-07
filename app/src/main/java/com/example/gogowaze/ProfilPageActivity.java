20add8bd65047877478d2204890a831a56fd02ac
victor dedieu <louis.victordedieu@gmail.com>
2023-06-07 16:07:24 +0200

    Ajout prise de photo pour faire une notif + fix profil page

--- app/src/main/java/com/example/gogowaze/ProfilPageActivity.java
+++ app/src/main/java/com/example/gogowaze/ProfilPageActivity.java
@@ -22,6 +22,8 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -51,6 +53,51 @@ public class ProfilPageActivity extends AppCompatActivity {
                 }
             }
         });
+        // Obtenez une référence au TabLayout et au ViewPager dans votre activité ou votre fragment
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        ViewPager viewPager = findViewById(R.id.view_pager);
+
+        // Créer un adaptateur de fragments pour le ViewPager
+        FragmentPagerAdapter adapter = new FragmentPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                // Retourner le fragment approprié pour chaque onglet
+                switch (position) {
+                    case 0:
+                        return new HistoriqueNotificationsFragment();
+                    case 1:
+                        return new HistoriqueSignalementsFragment();
+                    default:
+                        return null;
+                }
+            }
+            @Override
+            public int getCount() {
+                // Retourner le nombre total d'onglets
+                return 2;
+            }
+            @Nullable
+            @Override
+            public CharSequence getPageTitle(int position) {
+                // Retourner le titre de chaque onglet
+                switch (position) {
+                    case 0:
+                        return "Notifications";
+                    case 1:
+                        return "Signalements";
+                    default:
+                        return null;
+                }
+            }
+        };
+        // Attacher l'adaptateur de fragments au ViewPager
+        viewPager.setAdapter(adapter);
+
+        // Attacher le ViewPager au TabLayout
+        tabLayout.setupWithViewPager(viewPager);
+
+
         findViewById(R.id.buttonExit).setOnClickListener(
                 click -> {
                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
--- app/src/main/java/com/example/gogowaze/SignalFragment.java
+++ app/src/main/java/com/example/gogowaze/SignalFragment.java
@@ -1,17 +1,26 @@
 package com.example.gogowaze;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.Manifest;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -25,11 +34,15 @@ public class SignalFragment extends Fragment {
     private Button btnFermer;
     private Button btnAnnuler;
     private Button btnValider;
+    private Button btnCapture;
+    private Bitmap capturedImage = null;
+
     private RadioGroup radioGroupGravity;
     private RadioGroup radioGroupType;
     private TextInputEditText textTitle;
     private TextInputEditText textDescription;
-
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_CAMERA_PERMISSION = 2;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_signal, container, false);
@@ -37,6 +50,7 @@ public class SignalFragment extends Fragment {
         btnFermer = view.findViewById(R.id.buttonExit);
         btnAnnuler = view.findViewById(R.id.annulerSignalement);
         btnValider = view.findViewById(R.id.validateSignalement);
+        btnCapture = view.findViewById(R.id.buttonCapture);
         radioGroupGravity = view.findViewById(R.id.radioButtonGroupeGravity);
         radioGroupType = view.findViewById(R.id.radioButtonGroupeType);
         textTitle = view.findViewById(R.id.textTitre);
@@ -48,9 +62,38 @@ public class SignalFragment extends Fragment {
             createAndShowNotification();
             getActivity().getSupportFragmentManager().beginTransaction().remove(SignalFragment.this).commit();
         });
-
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ouvrir l'appareil photo
+                // Vérifier si l'application a l'autorisation d'utiliser l'appareil photo
+                if (ContextCompat.checkSelfPermission(SignalFragment.this.getContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(SignalFragment.this.getActivity(), new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                } else {
+                    openCamera();
+                }
+            }
+        });
         return view;
     }
+
+    private void openCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    // Override this method to get the result of the camera intent
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            capturedImage = (Bitmap) extras.get("data");
+        }
+    }
+
 
     public void createAndShowNotification() {
         int selectedGravityId = radioGroupGravity.getCheckedRadioButtonId();
@@ -64,7 +107,7 @@ public class SignalFragment extends Fragment {
         String titleText = textTitle.getText().toString();
         String descriptionText = textDescription.getText().toString();
 
-        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.bell_icon);
+        Bitmap largeIcon = capturedImage != null ? capturedImage : BitmapFactory.decodeResource(getResources(), R.drawable.bell_icon);
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this.getContext(), Notification.CHANNEL_ID)
                 .setSmallIcon(R.drawable.bell_icon)
@@ -84,5 +127,15 @@ public class SignalFragment extends Fragment {
         int notificationId = 1;  // This is just an example, you can set any unique number
 
         notificationManager.notify(notificationId, builder.build());
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera();
+            }
+        }
     }
 }
